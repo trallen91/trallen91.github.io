74c4a5dffcd292a152a311c178c319d7035292ea
trallen91 <travis.william.allen@gmail.com>
2015-08-26 21:28:58 -0400

    get fallDown feature to work after hitting the top

--- projects/myth_of_sisyphus/application.js
+++ projects/myth_of_sisyphus/application.js
@@ -1,29 +1,64 @@
 function backToStart() {
-  startRow = $("tr").last().index()
-  startColumn = $("td").first().index();
-  $startPosition = $("tr:eq("+startRow+") td:eq("+startColumn+")")
+  var startRow = $("tr").last().index()
+  var startColumn = $("td").first().index();
+  var $startPosition = $("tr:eq("+startRow+") td:eq("+startColumn+")")
   $startPosition.addClass("active")
   return $startPosition
 }
 
 function moveUp() {
-  currentColumn = $(".active").index()
-  currentRow = $(".active").parent().index()
-  newRow = (currentRow - 1)
-  newColumn = (currentColumn + 1)
-  $newPosition = $("tr:eq("+newRow+") td:eq("+newColumn+")")
-  // debugger
-  $currentPosition = $("tr:eq("+currentRow+") td:eq("+currentColumn+")")
+  var currentColumn = $(".active").index()
+  var currentRow = $(".active").parent().index()
+  var newRow = (currentRow - 1)
+  var newColumn = (currentColumn + 1)
+  var $newPosition = $("tr:eq("+newRow+") td:eq("+newColumn+")")
+  var $currentPosition = $("tr:eq("+currentRow+") td:eq("+currentColumn+")")
   $currentPosition.removeClass("active")
   $newPosition.addClass("active")
   return $newPosition
 }
 
+function findCurrentRow(){
+  var currentRow = $(".active").parent().index()
+  return currentRow
+
+}
+
+function fallDownOne() {
+  //logic to have it slide down diagonally
+  var currentColumn = $(".active").index()
+  var currentRow = $(".active").parent().index()
+  var newRow = (currentRow + 1)
+  var newColumn = (currentColumn - 1)
+  var $newPosition = $("tr:eq("+newRow+") td:eq("+newColumn+")")
+  var $currentPosition = $("tr:eq("+currentRow+") td:eq("+currentColumn+")")
+  $currentPosition.removeClass("active")
+  $newPosition.addClass("active")
+  return "It fell down one"
+}
+
+function fallDown() {
+  i = 1000
+  while (i < 10000) {
+
+  setTimeout(function() {
+    fallDownOne();
+  }, i);
+
+  i += 1000
+}
+
+};
+
 $(document).ready(function() {
   backToStart();
   $(document).on("keyup", function(event){
+    //sisyphus moves diagonal upon hitting button a
     if (event.keyCode == 65) {
-      moveUp()
-    };
+      moveUp();
+      if (findCurrentRow() === 0) {
+        fallDown();
+      };
+    }
   })
-})
+});
