89e6fdd291c2a9558bcc49d056ee9d1733747573
trallen91 <travis.william.allen@gmail.com>
2015-10-06 10:18:33 -0400

    Add tag input to Amazeriffic

--- projects/amazeriffic2/app.js
+++ projects/amazeriffic2/app.js
@@ -1,10 +1,31 @@
-var main = function () {
+var organizeByTags = function(toDoObjects) {
+  var tags = [];
+  toDoObjects.forEach(function(toDo) {
+    toDo.tags.forEach(function(tag) {
+      if (tags.indexOf(tag) === -1) {
+        tags.push(tag)
+      }
+    });
+  })
+  var tagObjects = tags.map(function(tag) {
+    var toDosWithTag = [];
+    toDoObjects.forEach(function(toDo) {
+      if(toDo.tags.indexOf(tag) !== -1) {
+        toDosWithTag.push(toDo.description)
+      }
+    });
+
+    return {"name": tag, "toDos": toDosWithTag}
+  });
+  return tagObjects;
+}
+
+var main = function (toDoObjects) {
   "use strict";
 
-  var toDos = [
-  "Buy a coffee",
-  "Choose a stack",
-  "Eat a snack"]
+  var toDos = toDoObjects.map(function(toDo) {
+    return toDo.description;
+  })
 
   $(".tabs span").toArray().forEach(function(element) {
     $(element).on("click", function() {
@@ -27,16 +48,37 @@ var main = function () {
           $content.append($("<li>").text(todo))
         });
         $("main .content").append($content)
-      } else if ($element.parent().is(":nth-child(3)")) {
-        $content = $("<section class='todo-input'><p>Add a todo to your list</p><input type = 'text'></section>")
+      } else if ($element.parent().is(":nth-child(3)")){
+        console.log("the tags tab was clicked")
+        var organizedByTag = organizeByTags(toDoObjects)
+
+        organizedByTag.forEach(function(tag) {
+          var $tagName = $("<h3>").text(tag.name),
+              $content = $("<ul>");
+          tag.toDos.forEach(function(description) {
+            var $li = $("<li>").text(description);
+            $content.append($li);
+          })
+
+          $("main .content").append($tagName);
+          $("main .content").append($content);
+        })
+      }
+      else if ($element.parent().is(":nth-child(4)")) {
+        $content = $("<section class='description-input'><p>Description</p><input type = 'text'></section><section class='tag-input'><p>Tag (Separate by commas, no spaces):</p><input type = 'text'></section><br><button>Submit</button>")
         $("main .content").append($content)
 
-        $(".todo-input").on("keypress", function(event) {
-          if (event.keyCode === 13) {
-            var todo = $(".todo-input input").val()
-            toDos.push(todo)
-            $(".todo-input input").val("")
-        }
+        $("button").on("click", function(event) {
+          var description = $(".description-input input").val()
+          var tags = $(".tag-input input").val().split(",");
+          toDoObjects.push({"description": description, "tags": tags});
+
+          toDos = toDoObjects.map(function(toDo) {
+            return toDo.description;
+          })
+
+          $(".description-input input").val("")
+          $(".tag-input input").val("")
         })
       }
 
@@ -48,4 +90,9 @@ var main = function () {
 
 }
 
-$(document).ready(main);
+$(document).ready(function(){
+  $.getJSON("todos.json", function(toDoObjects) {
+    console.log(toDoObjects)
+    main(toDoObjects)
+  })
+});
